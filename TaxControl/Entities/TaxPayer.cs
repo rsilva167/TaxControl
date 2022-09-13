9a871751c9b30c9476069377b6d5f696d087d64a
Renan Santos <dosantosrenansilva@gmail.com>
2022-09-12 21:07:43 -0300

    Tax control.

---
+++ TaxControl/Entities/TaxPayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxControl.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+
+        public double AnualIncome { get; set; }
+
+        public TaxPayer()
+        {
+
+        }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+        public abstract double Tax();
+    }
+}
